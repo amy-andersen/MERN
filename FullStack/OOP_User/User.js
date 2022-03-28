aa349403f35fea265f3ffa87a773091bc37f8f29
Amy Andersen <amyrandersen1@gmail.com>
2022-03-28 17:25:51 -0600

    Chain methods

--- FullStack/OOP_User/User.js
+++ FullStack/OOP_User/User.js
@@ -10,22 +10,26 @@ class User {
     //made deposit method
     makeDeposit(amount) {
         this.accountBalance += amount;
+        return this;
     }
 
     //make withdrawel method
     makeWithdrawel(amount) {
         this.accountBalance -= amount;
+        return this;
     }
 
     //display balance method
     displayBalance() {
         console.log(`User: ${this.name}, Balance: ${this.accountBalance}`)
+        return this;
     }
 
     //transfer money method
     transferMoney(amount, reciever) {
         this.accountBalance -= amount;
         reciever.accountBalance += amount;
+        return this;
     }
 
 }
@@ -36,24 +40,30 @@ const Hermione = new User("Hermione Granger", "HermG@gmail.com");
 const Ron = new User("Ron Weasley", "RWeasley@gmail.com");
 
 //first user makes 3 deposits and 1 withdrawel
-Harry.makeDeposit(100)
-Harry.makeDeposit(100)
-Harry.makeDeposit(100)
-Harry.makeWithdrawel(100)
-Harry.displayBalance()
+// Harry.makeDeposit(100)
+// Harry.makeDeposit(100)
+// Harry.makeDeposit(100)
+// Harry.makeWithdrawel(100)
+// Harry.displayBalance()
+//chained
+Harry.makeDeposit(100).makeDeposit(100).makeDeposit(100).makeWithdrawel(100).displayBalance()
 
 //second user makes 2 deposits and 2 withdrawels
-Hermione.makeDeposit(1000)
-Hermione.makeDeposit(1000)
-Hermione.makeWithdrawel(500)
-Hermione.displayBalance()
+// Hermione.makeDeposit(1000)
+// Hermione.makeDeposit(1000)
+// Hermione.makeWithdrawel(500)
+// Hermione.displayBalance()
+//chained
+Hermione.makeDeposit(1000).makeDeposit(1000).makeWithdrawel(500).displayBalance()
 
 //third user makes 1 deposit and 3 withdrawels
-Ron.makeDeposit(1000)
-Ron.makeWithdrawel(100)
-Ron.makeWithdrawel(100)
-Ron.makeWithdrawel(100)
-Ron.displayBalance()
+// Ron.makeDeposit(1000)
+// Ron.makeWithdrawel(100)
+// Ron.makeWithdrawel(100)
+// Ron.makeWithdrawel(100)
+// Ron.displayBalance()
+//chained
+Ron.makeDeposit(1000).makeWithdrawel(100).makeWithdrawel(100).makeWithdrawel(100).displayBalance()
 
 //first user transfers money to third user
 Harry.transferMoney(100, Hermione)
