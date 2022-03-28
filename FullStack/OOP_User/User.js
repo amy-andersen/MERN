13ba838454c9ee9638f19f80c7b0f5d84c833637
Amy Andersen <amyrandersen1@gmail.com>
2022-03-28 17:44:42 -0600

    Added Bank Account class

--- FullStack/OOP_User/User.js
+++ FullStack/OOP_User/User.js
@@ -34,6 +34,43 @@ class User {
 
 }
 
+//Create bank account class
+class BankAccount {
+    //define attributes
+    constructor(intRate, balance=0) {
+        this.intRate = intRate;
+        this.balance = balance;
+    }
+
+    //made deposit method
+    makeDeposit(amount) {
+        this.balance += amount;
+        return this;
+    }
+
+    //make withdrawel method
+    makeWithdrawel(amount) {
+        this.balance -= amount;
+        return this;
+    }
+
+    //display balance method
+    displayBalance() {
+        console.log(`Balance: ${this.balance}`)
+        return this;
+    }
+
+    //yield insterest method
+    yieldInterest() {
+        if(this.balance>0) {
+            this.balance += this.intRate*this.balance
+        }else {
+            this.balance = this.balance
+            }
+        return this;
+    }
+}
+
 //create 3 user instances
 const Harry = new User("Harry Potter", "Harry@aol.com");
 const Hermione = new User("Hermione Granger", "HermG@gmail.com");
@@ -45,6 +82,7 @@ const Ron = new User("Ron Weasley", "RWeasley@gmail.com");
 // Harry.makeDeposit(100)
 // Harry.makeWithdrawel(100)
 // Harry.displayBalance()
+
 //chained
 Harry.makeDeposit(100).makeDeposit(100).makeDeposit(100).makeWithdrawel(100).displayBalance()
 
@@ -53,6 +91,7 @@ Harry.makeDeposit(100).makeDeposit(100).makeDeposit(100).makeWithdrawel(100).dis
 // Hermione.makeDeposit(1000)
 // Hermione.makeWithdrawel(500)
 // Hermione.displayBalance()
+
 //chained
 Hermione.makeDeposit(1000).makeDeposit(1000).makeWithdrawel(500).displayBalance()
 
@@ -62,6 +101,7 @@ Hermione.makeDeposit(1000).makeDeposit(1000).makeWithdrawel(500).displayBalance(
 // Ron.makeWithdrawel(100)
 // Ron.makeWithdrawel(100)
 // Ron.displayBalance()
+
 //chained
 Ron.makeDeposit(1000).makeWithdrawel(100).makeWithdrawel(100).makeWithdrawel(100).displayBalance()
 
@@ -69,3 +109,13 @@ Ron.makeDeposit(1000).makeWithdrawel(100).makeWithdrawel(100).makeWithdrawel(100
 Harry.transferMoney(100, Hermione)
 Harry.displayBalance()
 Hermione.displayBalance()
+
+//create 2 bank accounts
+const Account1 = new BankAccount(0.02);
+const Account2 = new BankAccount(0.01, 100);
+
+//to the first account, make 3 deposits and 1 withdrawal, then calculate interest and display the account's info
+Account1.makeDeposit(100).makeDeposit(100).makeDeposit(100).makeWithdrawel(100).yieldInterest().displayBalance()
+
+//to the second account, make 2 deposits and 4 withdrawals, then calculate interest and display the account's info
+Account2.makeDeposit(900).makeDeposit(1000).makeWithdrawel(100).makeWithdrawel(100).makeWithdrawel(100).makeWithdrawel(100).yieldInterest().displayBalance()
